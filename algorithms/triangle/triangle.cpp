a120bda3a77fb7957df8bd6cc957ea8df8482440
Sean-Lan <sean.lan.thu@gmail.com>
2016-06-27 21:31:11 +0800

    Use dp to solve the triangle problem.

---
+++ algorithms/triangle/triangle.cpp
@@ -0,0 +1,41 @@
+/**
+ *
+ * Sean
+ * 2016-06-27
+ *
+ * https://leetcode.com/problems/triangle/
+ *
+ * Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+ *
+ * For example, given the following triangle
+ * [
+ *      [2],
+ *     [3,4],
+ *    [6,5,7],
+ *   [4,1,8,3]
+ * ]
+ * The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+ *
+ * Note:
+ * Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle.
+ *
+ */
+#include <vector>
+using namespace std;
+
+// dp
+// From bottom to up.
+// dp[j] = triangle[i][j] + min(dp[j], dp[j+1])
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        vector<int> dp = triangle.back();
+        for (int i = dp.size()-2; i>=0; --i) {
+            for (int j=0; j<=i; ++j)
+                dp[j] = triangle[i][j] + min(dp[j], dp[j+1]);
+        }
+        return dp[0];
+    }
+};
+
+int main() {}
