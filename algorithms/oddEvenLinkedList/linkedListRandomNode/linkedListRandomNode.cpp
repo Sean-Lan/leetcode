b8ed3d7359b8e7f8aebd43ddbf0c6d1bdb03b8e7
Sean-Lan <sean.lan.thu@gmail.com>
2016-08-14 10:45:16 +0800

    Use reservoir sampling to sovle the linked list random node problem.

---
+++ algorithms/oddEvenLinkedList/linkedListRandomNode/linkedListRandomNode.cpp
@@ -0,0 +1,65 @@
+/**
+ *
+ * Sean
+ * 2016-08-14
+ *
+ * https://leetcode.com/problems/linked-list-random-node/
+ *
+ * Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+ *
+ * Follow up:
+ * What if the linked list is extremely large and its length is unknown to you? Could you solve this efficiently without using extra space?
+ *
+ * Example:
+ *
+ * // Init a singly linked list [1,2,3].
+ * ListNode head = new ListNode(1);
+ * head.next = new ListNode(2);
+ * head.next.next = new ListNode(3);
+ * Solution solution = new Solution(head);
+ *
+ * // getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+ * solution.getRandom();
+ *
+ */
+#include <iostream>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// Reference to https://en.wikipedia.org/wiki/Reservoir_sampling
+class Solution {
+private:
+    ListNode *head;
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    Solution(ListNode* head) {
+        this->head = head;
+    }
+
+    /** Returns a random node's value. */
+    int getRandom() {
+        ListNode *result = head, *cur = head->next;
+        for (int i=2; cur; ++i, cur=cur->next) {
+            if (rand()%i == 0)
+                result = cur;
+        }
+        return result->val;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
+
+int main() {
+    return 0;
+}
