e310181385f41ba312bb97c703231d866e797356
Sean-Lan <sean.lan.thu@gmail.com>
2017-02-15 09:44:45 +0800

    use a stack to solve the next greater element II problem.

---
+++ algorithms/nextGreaterElementII/nextGreaterElementII.cpp
@@ -0,0 +1,55 @@
+/**
+ *
+ * Sean
+ * 2017-02-15
+ *
+ * https://leetcode.com/problems/next-greater-element-ii/
+ *
+ * Given a circular array (the next element of the last element is the first element of the array), print the Next Greater Number for every element. The Next Greater Number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, output -1 for this number.
+ *
+ * Example 1:
+ * Input: [1,2,1]
+ * Output: [2,-1,2]
+ * Explanation: The first 1's next greater number is 2;
+ * The number 2 can't find next greater number;
+ * The second 1's next greater number needs to search circularly, which is also 2.
+ * Note: The length of given array won't exceed 10000.
+ *
+ */
+#include <vector>
+#include <stack>
+using namespace std;
+
+// O(n), visit nums for two rounds
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        // pair<num, index>
+        stack<pair<int, int>> aStack;
+        int n = nums.size();
+        vector<int> res(n, -1);
+
+        int cur;
+        int round = 2;
+        while (round--) {
+            for (int i=0; i<n; ++i) {
+                cur = nums[i];
+                while (!aStack.empty()) {
+                    auto top = aStack.top();
+                    if (top.first < cur) {
+                        res[top.second] = cur;
+                        aStack.pop();
+                    } else {
+                        break;
+                    }
+                }
+                if (round) aStack.push({cur, i});
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    return 0;
+}
