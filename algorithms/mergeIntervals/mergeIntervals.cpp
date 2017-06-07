7dca64091b7f5eb9188321573b87f22ab65f787c
Sean-Lan <sean.lan.thu@gmail.com>
2017-06-07 15:26:04 +0800

    tweak the interval merging problem

--- algorithms/mergeIntervals/mergeIntervals.cpp
+++ algorithms/mergeIntervals/mergeIntervals.cpp
@@ -26,48 +26,34 @@ struct Interval {
 };
 
 // First sort, then iterate over intervals, check whether currentInterval and preInterval overlap.
-// If so, merge them, otherwise, put the smaller one into the result vector.
+// If so, merge them, otherwise, put the previous one into the result vector because it ends first
 class Solution {
-    private:
-        bool overlap(const Interval &interval1, const Interval &interval2) {
-            if (interval1.start>interval2.end || interval2.start>interval1.end)
-                return false;
-            return true;
-        }
-
-        Interval mergeInterval(const Interval &interval1, const Interval &interval2) {
-            return {
-                min(interval1.start, interval2.start),
-                max(interval1.end, interval2.end)
-            };
-        }
 public:
     vector<Interval> merge(vector<Interval>& intervals) {
         if (intervals.empty()) return {};
-        sort(intervals.begin(), intervals.end(),
-                [](Interval &interval1, Interval &interval2) {
-                    return (interval1.start!=interval2.start)
-                           ?(interval1.start<interval2.start)
-                           :(interval1.end<interval2.end);
-                });
+        sort(intervals.begin(), intervals.end(), [](const Interval& i1, const Interval& i2) {
+            if (i1.start != i2.start) return i1.start < i2.start;
+            return i1.end < i2.end;
+        });
+        
+        Interval prev = intervals[0];
+        int n = intervals.size();
         vector<Interval> res;
-        Interval prevInterval = intervals[0];
-        for (size_t i=1; i<intervals.size(); ++i) {
-            Interval &currentInterval = intervals[i];
-            if (overlap(prevInterval, currentInterval))
-                prevInterval = mergeInterval(prevInterval, currentInterval);
-            else {
-                if (prevInterval.end < currentInterval.start) {
-                    res.push_back(prevInterval);
-                    prevInterval = currentInterval;
-                } else
-                    res.push_back(currentInterval);
+        for (int i=1; i<n; ++i) {
+            auto &cur = intervals[i];
+            if (cur.start <= prev.end) { // overlap
+                prev.end = max(prev.end, cur.end);
+            } else {
+                res.push_back(prev);
+                prev.start = cur.start;
+                prev.end = cur.end;
             }
         }
-        res.push_back(prevInterval);
+        res.push_back(prev);
         return res;
     }
 };
 
 int main() {
+    return 0;
 }
