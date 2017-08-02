959c0a2fa2fb6966533ec8c7c1d1fe8ff71670bb
Sean-Lan <sean.lan.thu@gmail.com>
2017-08-02 09:47:57 +0800

    fix the bug in wordBreak dp solution

--- algorithms/wordBreak/wordBreak2.cpp
+++ algorithms/wordBreak/wordBreak2.cpp
@@ -26,7 +26,7 @@ class Solution
     public:
         bool wordBreak(string s, unordered_set<string>& wordDict) {
             int len = s.size();
-            vector<bool> dp(len, false);
+            vector<bool> dp(len+1, false);
             dp[0] = true;
             for (int i=1; i<=len; ++i)
                 for (int j=i-1; j>=0; --j)
