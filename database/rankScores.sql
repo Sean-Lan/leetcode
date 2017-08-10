a02bf792afd2cc324608b4872e3c199f56939a8f
Sean-Lan <sean.lan.thu@gmail.com>
2017-08-10 12:01:07 +0800

    sovle the tricky rank score problem

---
+++ database/rankScores.sql
@@ -0,0 +1,40 @@
+--
+-- Sean
+-- 2017-08-10
+-- 
+-- https://leetcode.com/problems/rank-scores/discuss/
+-- 
+-- Write a SQL query to rank scores. If there is a tie between two scores, both should have the same ranking. Note that after a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no "holes" between ranks.
+-- 
+-- +----+-------+
+-- | Id | Score |
+-- +----+-------+
+-- | 1  | 3.50  |
+-- | 2  | 3.65  |
+-- | 3  | 4.00  |
+-- | 4  | 3.85  |
+-- | 5  | 4.00  |
+-- | 6  | 3.65  |
+-- +----+-------+
+-- For example, given the above Scores table, your query should generate the following report (order by highest score):
+-- 
+-- +-------+------+
+-- | Score | Rank |
+-- +-------+------+
+-- | 4.00  | 1    |
+-- | 4.00  | 1    |
+-- | 3.85  | 2    |
+-- | 3.65  | 3    |
+-- | 3.65  | 3    |
+-- | 3.50  | 4    |
+-- +-------+------+
+--
+
+-- take advantage of a tmp table about the distint score
+SELECT Score, (
+  SELECT count(*)
+  FROM (SELECT distinct Score AS S FROM Scores) AS tmp
+  WHERE tmp.S >= Score
+) AS Rank
+FROM Scores
+ORDER BY Score DESC;
