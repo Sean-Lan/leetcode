1e1c751ca94eafeb94bedb06f17cab56e19cbfad
Sean-Lan <sean.lan.thu@gmail.com>
2017-08-22 20:32:51 +0800

    add an elegant solution to intersectionOfTwoLinkedLists problem

---
+++ algorithms/intersectionOfTwoLinkedLists/intersectionOfTwoLinkedLists2.cpp
@@ -0,0 +1,54 @@
+/**
+ *
+ * Sean
+ * 2017-08-22
+ *
+ * https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+ *
+ * Write a program to find the node at which the intersection of two singly linked lists begins.
+ *
+ * For example, the following two linked lists:
+ *
+ * A:          a1 → a2
+ *                    ↘
+ *                      c1 → c2 → c3
+ *                    ↗
+ * B:     b1 → b2 → b3
+ * begin to intersect at node c1.
+ *
+ *
+ * Notes:
+ *
+ * If the two linked lists have no intersection at all, return null.
+ * The linked lists must retain their original structure after the function returns.
+ * You may assume there are no cycles anywhere in the entire linked structure.
+ * Your code should preferably run in O(n) time and use only O(1) memory.
+ *
+ */
+#include <iostream>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// O(n)
+// visit listA and listB at the same time, when meet one list's end, starts visting another list
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *pa = headA, *pb = headB;
+        while (pa != pb) {
+            pa = pa ? pa->next : headB;
+            pb = pb ? pb->next : headA;
+        }
+        return pa;
+    }
+};
+
+int main() {
+    return 0;
+}
