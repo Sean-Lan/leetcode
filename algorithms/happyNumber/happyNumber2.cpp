dd7054eddc66806c6dade03d5e058525fbe91aeb
Sean-Lan <sean.lan.thu@gmail.com>
2017-08-29 10:20:32 +0800

    use two pointers to solve the happy number problem

--- algorithms/happyNumber/happyNumber2.cpp
+++ algorithms/happyNumber/happyNumber2.cpp
@@ -1,11 +1,7 @@
 /**
  *
  * Sean
- * 2016-06-15
- *
- * https://leetcode.com/problems/happy-number/
- *
- * Write an algorithm to determine if a number is "happy".
+ * 2017-08-29
  *
  * A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
  *
@@ -18,27 +14,33 @@
  *
  */
 #include <iostream>
-#include <unordered_set>
+#include <vector>
 using namespace std;
 
+// two pointers to check cycle
 class Solution {
-    public:
-        bool isHappy(int n) {
-            unordered_set<int> nums;
-
-            int tmp;
-            while (nums.count(n)==0) {
-                nums.insert(n);
-                tmp = 0;
-                while (n) {
-                    tmp += (n%10) * (n%10);
-                    n /= 10;
-                }
-                n = tmp;
-            }
-
-            return nums.count(1);
+    int squareSum(int n) {
+        int sum = 0, r;
+        while (n) {
+            r = n % 10;
+            n /= 10;
+            sum += r*r;
         }
+        return sum;
+    }
+public:
+    bool isHappy(int n) {
+        int slow, fast;
+        slow = fast = n;
+        do {
+            slow = squareSum(slow);
+            fast = squareSum(fast);
+            fast = squareSum(fast);
+        } while (slow != fast);
+        return slow == 1;
+    }
 };
 
-int main() {}
+int main() {
+    return 0;
+}
