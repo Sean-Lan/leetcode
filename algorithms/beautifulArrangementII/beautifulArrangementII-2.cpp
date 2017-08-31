5b8ae1f6fbb8ee5ae0dd00e54ebd5ed6b10bf8b7
Sean-Lan <sean.lan.thu@gmail.com>
2017-08-31 11:39:56 +0800

    use list to make the insertion O(1) & overall complexity O(n)

--- algorithms/beautifulArrangementII/beautifulArrangementII-2.cpp
+++ algorithms/beautifulArrangementII/beautifulArrangementII-2.cpp
@@ -14,39 +14,40 @@
  * Input: n = 3, k = 1
  * Output: [1, 2, 3]
  * Explanation: The [1, 2, 3] has three different positive integers ranging from 1 to 3, and the [1, 1] has exactly 1 distinct integer: 1.
- *
  * Example 2:
  * Input: n = 3, k = 2
  * Output: [1, 3, 2]
  * Explanation: The [1, 3, 2] has three different positive integers ranging from 1 to 3, and the [2, 1] has exactly 2 distinct integers: 1 and 2.
- *
  * Note:
  * The n and k are in the range 1 <= k < n <= 10^4.
  *
  */
-#include <iostream>
 #include <vector>
+#include <list>
 #include <numeric>
 using namespace std;
 
-// O(n^2)
+// O(n) with list
 // first make an array as 1..n
 // move the smallest number before the largest one, and do it recursively
 // k -> k-2
 class Solution {
-    void sovle(int k, vector<int> &nums) {
+    typedef list<int>::iterator It;
+    void sovle(int k, list<int> &nums, It p) {
         if (k <= 1) return;
-        for (int i=0; i<k-1; ++i) {
-            swap(nums[i], nums[i+1]);
-        }
-        sovle(k-2, nums);
+        nums.insert(p, nums.front());
+        nums.erase(nums.begin());
+        --p; --p;
+        sovle(k-2, nums, p);
     }
 public:
     vector<int> constructArray(int n, int k) {
-        vector<int> nums(n, 0);
+        list<int> nums(n, 0);
         iota(nums.begin(), nums.end(), 1);
-        sovle(k, nums);
-        return nums;
+        It p = nums.begin();
+        for (int i=0; i<k; ++i) ++p;
+        sovle(k, nums, p);
+        return vector<int>(nums.begin(), nums.end());
     }
 };
 
