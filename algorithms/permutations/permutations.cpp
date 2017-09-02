bb4dfab0bc1789ffcacba61c7079a4eb13c90bb5
Sean-Lan <sean.lan.thu@gmail.com>
2017-09-02 12:23:59 +0800

    tweak the solution for permutations

--- algorithms/permutations/permutations.cpp
+++ algorithms/permutations/permutations.cpp
@@ -1,47 +1,56 @@
+/**
+ *
+ * Sean
+ * 2017-09-02
+ *
+ * https://leetcode.com/problems/permutations/description/
+ *
+ * Given a collection of distinct numbers, return all possible permutations.
+ *
+ * For example,
+ * [1,2,3] have the following permutations:
+ * [
+ *   [1,2,3],
+ *   [1,3,2],
+ *   [2,1,3],
+ *   [2,3,1],
+ *   [3,1,2],
+ *   [3,2,1]
+ * ]
+ *
+ */
 #include <iostream>
 #include <vector>
 using namespace std;
 
+// typical backtracking
 class Solution {
-    private:
-        void dfs(vector<int> &nums, int step, vector<bool> &used,
-                vector<int> &currentResult, vector<vector<int>> &results) {
-            if (step == nums.size()) {
-                results.push_back(currentResult);
-                return;
-            }
-            for (int i=0; i<nums.size(); ++i) {
-                if (!used[i]) {
-                    used[i] = true;
-                    currentResult.push_back(nums[i]);
-                    dfs(nums, step+1, used, currentResult, results);
-                    used[i] = false;
-                    currentResult.pop_back();
-                }
+    void sovle(vector<vector<int>> &res, vector<int> &path, vector<int> &nums, vector<bool> &mark) {
+        if (path.size() == nums.size()) {
+            res.push_back(path);
+            return;
+        }
+        for (int i=0; i<nums.size(); ++i) {
+            if (!mark[i]) {
+                mark[i] = true;
+                path.push_back(nums[i]);
+                sovle(res, path, nums, mark);
+                path.pop_back();
+                mark[i] = false;
             }
         }
-
-    public:
-        vector<vector<int>> permute(vector<int>& nums) {
-            if (nums.empty()) return {};
-            vector<int> currentResult;
-            vector<vector<int>> results;
-            vector<bool> used(nums.size());
-            dfs(nums, 0, used, currentResult, results);
-            return results;
-        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> mark(n, false);
+        vector<int> path;
+        vector<vector<int>> res;
+        sovle(res, path, nums, mark);
+        return res;
+    }
 };
 
-void printResults(const vector<vector<int>> &results) {
-    for (auto &iv : results) {
-        for (auto &i : iv)
-            cout << i << " ";
-        cout << endl;
-    }
-}
-
 int main() {
-    vector<int> nums = {1,2,3};
-    Solution solution;
-    printResults(solution.permute(nums));
+    return 0;
 }
