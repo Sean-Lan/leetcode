9fe222f7e93891572cced0541a05f34c4ee8fd7f
xiang_lan <xiang.lan@airbnb.com>
2020-04-18 11:46:09 +0800

    re-format

--- algorithms/trappingRainWater/trappingRainWater4.cpp
+++ algorithms/trappingRainWater/trappingRainWater4.cpp
@@ -20,34 +20,34 @@ using namespace std;
 // DP, O(n) time & space complexity
 // calculate leftMax & rightMax separately
 class Solution {
-public:
-int trap(vector<int>& height) {
-        int n = height.size();
-        if (n <= 1) return 0;
+  public:
+    int trap(vector<int>& height) {
+      int n = height.size();
+      if (n <= 1) return 0;
 
-        vector<int> leftMax(n, 0);
-        vector<int> rightMax(n, 0);
+      vector<int> leftMax(n, 0);
+      vector<int> rightMax(n, 0);
 
-        leftMax[0] = height[0];
-        rightMax[n-1] = height[n-1];
+      leftMax[0] = height[0];
+      rightMax[n-1] = height[n-1];
 
-        for (int i=1; i<n; ++i) {
-                    leftMax[i] = max(leftMax[i-1], height[i]);
-                }
-        for (int i=n-2; i>=0; --i) {
-                    rightMax[i] = max(rightMax[i+1], height[i]);
-                }
+      for (int i=1; i<n; ++i) {
+        leftMax[i] = max(leftMax[i-1], height[i]);
+      }
+      for (int i=n-2; i>=0; --i) {
+        rightMax[i] = max(rightMax[i+1], height[i]);
+      }
 
-        int total = 0;
-        for (int i=0; i<n-1; ++i) {
-                    int minH = min(leftMax[i], rightMax[i]);
-                    if (minH > height[i]) total += minH - height[i];
-                }
+      int total = 0;
+      for (int i=0; i<n-1; ++i) {
+        int minH = min(leftMax[i], rightMax[i]);
+        if (minH > height[i]) total += minH - height[i];
+      }
 
-        return total;
+      return total;
     }
 };
 
 int main() {
-    return 0;
+  return 0;
 }
